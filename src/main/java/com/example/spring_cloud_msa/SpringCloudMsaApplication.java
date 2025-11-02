b314f31dc1331ef4b1804635ffae78d2914c251b
jaspresso <jaspresso@gmail.com>
2025-11-02 10:55:08 +0900

    초기화 작업

---
+++ src/main/java/com/example/spring_cloud_msa/SpringCloudMsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMsaApplication.class, args);
+	}
+
+}
